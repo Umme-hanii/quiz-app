639cfe0b576a729827ba57eb8bbfb304aaa1cca7
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-15 23:48:54 +0530

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { WelcomeComponent } from './welcome/welcome.component'
-import { QuestionComponent } from './question/question.component'
-import { HeaderComponent } from './header/header.component'
+import { WelcomeComponent } from './components/welcome/welcome.component'
+import { QuestionComponent } from './components/question/question.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
